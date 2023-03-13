6a69c5f95a50dec4f4c6c5c020483518c6f0eac4
AndrewIndeche <indecheandrew@outlook.com>
2023-03-13 23:08:12 +0300

    add Homepage to routes

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Homepage from './components/Homepage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Login/>}/>
+        <Route path="/Homepage" element={<Homepage/>} /> 
       </Routes>
     </Router>
     );
