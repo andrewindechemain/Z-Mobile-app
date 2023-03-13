45f18b427af8ae21755b968c52c844bafeeae373
AndrewIndeche <indecheandrew@outlook.com>
2023-03-13 08:14:49 +0300

    add Login to routes

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login'
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route exact path="/" element={<Login/>}/>
       </Routes>
     </Router>
     );
