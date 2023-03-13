58097101de11b634e03c6918f80c16eb9b8387a8
AndrewIndeche <indecheandrew@outlook.com>
2023-03-13 17:28:48 +0300

    add font awesome icons

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons/faUser'
+import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
+import LoginDashboard from '../components/LoginDashboard'
 
 export default function app(){
     return(
         <>  
         <View style={styles.container}>
         <Image source={require('../assets/logo.png')} style={styles.imgstyle} />
-        <FontAwesomeIcon icon={ faUser } color={ 'white' } size={ 50 } />
+        <br/>
+        <FontAwesomeIcon icon={ faUser } style={styles.fontawesome} color={ 'white' } size={ 50 } />
         <h2 >Welcome</h2>
         <h3>Log-in to continue</h3>
+        <br/>
+        <LoginDashboard/>
         </View>
         </>
     );
 }
 const styles = StyleSheet.create({
+    body: {
+        fontFamily: 'TsukimRounded-Medium',
+    },
     container: {
       flex: 1,
       width:'100%',
@@ -23,14 +30,17 @@ const styles = StyleSheet.create({
       color: 'white',
       alignItems: 'center',
       justifyContent: 'center',
-      paddingBottom: '7em'
+      paddingBottom: '1em',
     },
     imgstyle: {
         resizeMode:'contain',
         position:'absolute', 
         top:'0px',
-        marginTop:'0px',
+        marginTop:'-4em',
         height:'14em',
         width:'14em',
+      },
+      fontawesome: {
+        paddingTop: '8em'
       },
   });
