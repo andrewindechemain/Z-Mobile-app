9b9aabd93a569095657e69016d44fb53dbe19bbe
AndrewIndeche <indecheandrew@outlook.com>
2023-04-04 21:01:19 +0300

    add react-test-renderer

---
+++ __tests__/Continents-test.jsx
@@ -0,0 +1,8 @@
+import React from "react"; 
+import renderer from 'react-test-renderer';
+import Continents from './components/Continents.jsx';
+
+test('renders correctly', () =>{
+    const tree = renderer.create( <Continents />).toJson();
+    expect(tree).toMatchSnapshot();
+});
